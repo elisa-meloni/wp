2c2a48b494ed77febb4f7f25ca80a409f5ea2c1b
elisa-meloni <elisameloni86@gmail.com>
2018-02-07 17:27:54 +0100

    Add files via upload

---
+++ wp-content/themes/Elisa Tema/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <?php wp_head(); ?><meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    
+</head>
+<body>
+    <div class="jumbotron">
+      <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"  data-target="#my-menu" aria espanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#"></a>
+                </div>
+                <ul class="nav navbar-nav navbar-right collapse navbar-collapse" id="my-menu">
+                    <li>
+                        <a href="#" data-toggle="dropdown">Home<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Chi sono</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" data-toggle="dropdown">Fotografia<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Mare del Nord</a></li>
+                            <li><a href="#">Cuore di Cane</a></li>
+                            <li><a href="#">Gabbiani</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" data-toggle="dropdown">Video<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Resilient</a></li>
+                            <li><a href="#">Atmospheres</a></li>
+                            <li><a href="#">Demo</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" data-toggle="dropdown">Contatti<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#contact">social</a></li>
+                           <li><a href="#contact">cv</a></li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
---
+++ wp-content/themes/Elisa Tema/index.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+      <div class="row">
+        <div class="col-md-12 welcome">
+          <h1><?php bloginfo(name) ?></h1>
+          <h4><?php bloginfo(description) ?></h4>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row text-center" id="contact">
+<?php get_footer(); ?>
