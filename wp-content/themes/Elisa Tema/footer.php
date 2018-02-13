9611a0de717ae9911d0d3e3cdda459163286503a
elisa-meloni <elisameloni86@gmail.com>
2018-02-14 00:58:39 +0100

    Update footer.php

--- wp-content/themes/Elisa Tema/footer.php
+++ wp-content/themes/Elisa Tema/footer.php
@@ -1,3 +1,15 @@
 <?php wp_footer();?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html><footer>
+	<small>tutte le immagini sono protette da copyright © febbraio 2018</small>
+</footer>
+
 </body>
 </html>
